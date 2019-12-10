43796289f092342572eaa75328330790d1ca3488
PunitLodha <punitlodha@pm.me>
2019-12-10 19:47:28 +0530

    added movies section

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import {
+  getTopRatedMovies,
+  getPopularMovies,
+  getNowPlayingMovies,
+  getUpcomingMovies,
+} from '../api/TmdbAPI';
+import ItemGrid from './ItemGrid';
 
 const Movies = () => {
-  return <h1>Movies</h1>;
+  const [movies, setmovies] = useState([]);
+
+  useEffect(() => {
+    getTopRatedMovies().then(({ data }) => {
+      setmovies(prevState => ({
+        ...prevState,
+        topRated: data.results,
+      }));
+    });
+
+    getPopularMovies().then(({ data }) => {
+      setmovies(prevState => ({
+        ...prevState,
+        popular: data.results,
+      }));
+    });
+
+    getUpcomingMovies().then(({ data }) => {
+      setmovies(prevState => ({
+        ...prevState,
+        upcoming: data.results,
+      }));
+    });
+
+    getNowPlayingMovies().then(({ data }) => {
+      setmovies(prevState => ({
+        ...prevState,
+        nowPlaying: data.results,
+      }));
+    });
+  }, []);
+  return (
+    <div>
+      {movies.topRated && movies.popular && movies.nowPlaying && movies.upcoming ? (
+        <>
+          <ItemGrid items={movies.topRated} type="movie" name="Top Rated" />
+          <ItemGrid items={movies.nowPlaying} type="movie" name="Now Playing" />
+          <ItemGrid items={movies.popular} type="movie" name="Popular" />
+          <ItemGrid items={movies.upcoming} type="movie" name="Upcoming" />
+        </>
+      ) : (
+        <Typography variant="h3" color="secondary">
+          Loading
+        </Typography>
+      )}
+    </div>
+  );
 };
 
 export default Movies;
