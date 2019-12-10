748606ebf0ecb0aed1f8f76d47142667da188115
PunitLodha <punitlodha@pm.me>
2019-12-10 22:14:03 +0530

    added Tv component

--- src/components/Tv.js
+++ src/components/Tv.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import {
+  getOnTheAirTvShows,
+  getTopRatedTvShows,
+  getPopularTvShows,
+  getAiringTodayTvShows,
+} from '../api/TmdbAPI';
+import ItemGrid from './ItemGrid';
 
 const Tv = () => {
-  return <h1>Tv</h1>;
+  const [tvShows, settvShows] = useState([]);
+
+  useEffect(() => {
+    getTopRatedTvShows().then(({ data }) => {
+      settvShows(prevState => ({
+        ...prevState,
+        topRated: data.results,
+      }));
+    });
+
+    getPopularTvShows().then(({ data }) => {
+      settvShows(prevState => ({
+        ...prevState,
+        popular: data.results,
+      }));
+    });
+
+    getOnTheAirTvShows().then(({ data }) => {
+      settvShows(prevState => ({
+        ...prevState,
+        onTheAir: data.results,
+      }));
+    });
+
+    getAiringTodayTvShows().then(({ data }) => {
+      settvShows(prevState => ({
+        ...prevState,
+        airingToday: data.results,
+      }));
+    });
+  }, []);
+  return (
+    <div>
+      {tvShows.topRated && tvShows.popular && tvShows.onTheAir && tvShows.airingToday ? (
+        <>
+          <ItemGrid items={tvShows.topRated} type="TV" name="Top Rated" />
+          <ItemGrid items={tvShows.onTheAir} type="TV" name="On The Air" />
+          <ItemGrid items={tvShows.popular} type="TV" name="Popular" />
+          <ItemGrid items={tvShows.airingToday} type="TV" name="Airing Today" />
+        </>
+      ) : (
+        <Typography variant="h3" color="secondary">
+          Loading
+        </Typography>
+      )}
+    </div>
+  );
 };
 
 export default Tv;
