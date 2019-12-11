80a5e20c97ba34b80d163a7074f6d8dd3aa6e8ae
PunitLodha <punitlodha@pm.me>
2019-12-11 19:12:19 +0530

    removed api calls and fixed type

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,59 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Typography } from '@material-ui/core';
-import {
-  getTopRatedMovies,
-  getPopularMovies,
-  getNowPlayingMovies,
-  getUpcomingMovies,
-} from '../api/TmdbAPI';
 import ItemGrid from './ItemGrid';
 import { MoviesContext } from './MoviesContext';
 
 const Movies = () => {
   const { movies } = useContext(MoviesContext);
-  /* const [movies, setmovies] = useState([]);
-
-  useEffect(() => {
-    getTopRatedMovies().then(({ data }) => {
-      setmovies(prevState => ({
-        ...prevState,
-        topRated: data.results,
-      }));
-    });
-
-    getPopularMovies().then(({ data }) => {
-      setmovies(prevState => ({
-        ...prevState,
-        popular: data.results,
-      }));
-    });
-
-    getUpcomingMovies().then(({ data }) => {
-      setmovies(prevState => ({
-        ...prevState,
-        upcoming: data.results,
-      }));
-    });
-
-    getNowPlayingMovies().then(({ data }) => {
-      setmovies(prevState => ({
-        ...prevState,
-        nowPlaying: data.results,
-      }));
-    });
-  }, []); */
 
   return (
     <div>
       {movies.topRated && movies.popular && movies.nowPlaying && movies.upcoming ? (
         <>
-          <ItemGrid items={movies.topRated} type="movie" name="Top Rated" />
-          <ItemGrid items={movies.nowPlaying} type="movie" name="Now Playing" />
-          <ItemGrid items={movies.popular} type="movie" name="Popular" />
-          <ItemGrid items={movies.upcoming} type="movie" name="Upcoming" />
+          <ItemGrid items={movies.topRated} type="movies" name="Top Rated" />
+          <ItemGrid items={movies.nowPlaying} type="movies" name="Now Playing" />
+          <ItemGrid items={movies.popular} type="movies" name="Popular" />
+          <ItemGrid items={movies.upcoming} type="movies" name="Upcoming" />
         </>
       ) : (
-        <Typography variant="h3" color="secondary">
+        <Typography variant="h3" color="secondary" align="center">
           Loading
         </Typography>
       )}
--- src/components/Tv.js
+++ src/components/Tv.js
@@ -1,59 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Typography } from '@material-ui/core';
-import {
-  getOnTheAirTvShows,
-  getTopRatedTvShows,
-  getPopularTvShows,
-  getAiringTodayTvShows,
-} from '../api/TmdbAPI';
 import ItemGrid from './ItemGrid';
 import { MoviesContext } from './MoviesContext';
 
 const Tv = () => {
   const { tvShows } = useContext(MoviesContext);
-  /* const [tvShows, settvShows] = useState([]);
-
-  useEffect(() => {
-    getTopRatedTvShows().then(({ data }) => {
-      settvShows(prevState => ({
-        ...prevState,
-        topRated: data.results,
-      }));
-    });
-
-    getPopularTvShows().then(({ data }) => {
-      settvShows(prevState => ({
-        ...prevState,
-        popular: data.results,
-      }));
-    });
-
-    getOnTheAirTvShows().then(({ data }) => {
-      settvShows(prevState => ({
-        ...prevState,
-        onTheAir: data.results,
-      }));
-    });
-
-    getAiringTodayTvShows().then(({ data }) => {
-      settvShows(prevState => ({
-        ...prevState,
-        airingToday: data.results,
-      }));
-    });
-  }, []); */
 
   return (
     <div>
       {tvShows.topRated && tvShows.popular && tvShows.onTheAir && tvShows.airingToday ? (
         <>
-          <ItemGrid items={tvShows.topRated} type="TV" name="Top Rated" />
-          <ItemGrid items={tvShows.onTheAir} type="TV" name="On The Air" />
-          <ItemGrid items={tvShows.popular} type="TV" name="Popular" />
-          <ItemGrid items={tvShows.airingToday} type="TV" name="Airing Today" />
+          <ItemGrid items={tvShows.topRated} type="tv" name="Top Rated" />
+          <ItemGrid items={tvShows.onTheAir} type="tv" name="On The Air" />
+          <ItemGrid items={tvShows.popular} type="tv" name="Popular" />
+          <ItemGrid items={tvShows.airingToday} type="tv" name="Airing Today" />
         </>
       ) : (
-        <Typography variant="h3" color="secondary">
+        <Typography variant="h3" color="secondary" align="center">
           Loading
         </Typography>
       )}
