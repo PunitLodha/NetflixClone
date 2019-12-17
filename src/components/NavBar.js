d673b9bb7ed9189e709c247174973513e353a64b
PunitLodha <punitlodha@pm.me>
2019-12-17 15:43:47 +0530

    added switch for toggling theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { AppBar, Toolbar, Typography, FormControlLabel, Switch } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
 import logo from '../NetflixLogo.svg';
+import { ThemeContext } from './ThemeContext';
 
 const useStyles = makeStyles({
+  appBar: {
+    backgroundColor: theme => `${theme.background}`,
+  },
   nav: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     height: '5rem',
+    width: '180rem',
     padding: '0 2rem',
     listStyle: 'none',
   },
   linkStyle: {
-    color: '#ccc',
+    color: theme => `${theme.text}`,
     textDecoration: 'none',
     margin: '0 2rem',
     '&:hover': {
-      color: 'white',
+      color: theme => `${theme.secondary}`,
     },
     '&.active': {
-      color: 'white',
+      color: theme => `${theme.secondary}`,
     },
   },
   img: {
     width: '10rem',
     margin: '0.6rem 2rem 0 0',
   },
+  label: {
+    color: theme => `${theme.secondary}`,
+  },
 });
 
 const NavBar = () => {
-  const classes = useStyles();
+  const { isLight, dark, light, handleChange } = useContext(ThemeContext);
+  const theme = isLight ? light : dark;
+  const classes = useStyles(theme);
   return (
     <div>
-      <AppBar position="sticky">
+      <AppBar position="sticky" className={classes.appBar}>
         <Toolbar>
           <ul className={classes.nav}>
             <li>
@@ -55,11 +65,18 @@ const NavBar = () => {
                 <Typography variant="h6">TV Shows</Typography>
               </li>
             </NavLink>
-            <NavLink className={classes.linkStyle} to="/discover">
+            <NavLink className={classes.linkStyle} to="/discover" style={{ marginRight: 'auto' }}>
               <li>
                 <Typography variant="h6">Discover</Typography>
               </li>
             </NavLink>
+            <FormControlLabel
+              control={<Switch checked={!isLight} onChange={handleChange} />}
+              label="DarkMode"
+              classes={{
+                label: classes.label,
+              }}
+            />
           </ul>
         </Toolbar>
       </AppBar>
