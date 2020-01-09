2f546dd5d0c851eab8d923ee54e45382e4a235b2
PunitLodha <punitlodha@pm.me>
2020-01-09 22:24:37 +0530

    Updates

---
+++ precache-manifest.8333b85019c1566a4fd8c7ed28af2d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1c87cc9fe46bd23bce629564355d43",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "bb83f781e90f2a2b7d4a",
+    "url": "/NetflixClone/static/css/main.6d692180.chunk.css"
+  },
+  {
+    "revision": "ef34f0e8a455f20da9eb",
+    "url": "/NetflixClone/static/js/2.bde51a9d.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/NetflixClone/static/js/2.bde51a9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb83f781e90f2a2b7d4a",
+    "url": "/NetflixClone/static/js/main.396e223c.chunk.js"
+  },
+  {
+    "revision": "b2f5987bad533cfceaec",
+    "url": "/NetflixClone/static/js/runtime-main.65eeb454.js"
+  },
+  {
+    "revision": "ab665ffa34cef6f3d80b1e43ee6902ce",
+    "url": "/NetflixClone/static/media/NetflixLogo.ab665ffa.svg"
+  }
+]);
